bb16c981ce120b3881db28cf9d0bb1a6dda236c0
supunkamburugamuva <supun06@gmail.com>
2014-08-07 12:49:10 -0400

    adding zookeeper

--- sensorstream-core/src/main/java/cgl/sensorstream/core/SpoutCreator.java
+++ sensorstream-core/src/main/java/cgl/sensorstream/core/SpoutCreator.java
@@ -3,7 +3,8 @@ package cgl.sensorstream.core;
 import backtype.storm.spout.ISpout;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SpoutCreator {
-    public ISpout create(String url, List<String> fields, );
+    public ISpout create(String url, List<String> fields, Object convertor, Map<String, String> properties);
 }
